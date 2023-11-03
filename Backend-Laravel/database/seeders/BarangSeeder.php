7e235aee02238e844d44d2669778628ea7041496
faizalagathon <faizalagathon06@gmail.com>
2023-11-03 22:59:37 +0700

    setting seeder

--- Backend-Laravel/database/seeders/BarangSeeder.php
+++ Backend-Laravel/database/seeders/BarangSeeder.php
@@ -13,6 +13,59 @@ class BarangSeeder extends Seeder
      */
     public function run(): void
     {
-        Barang::factory(9)->create();
+        Barang::create([
+            'nama_barang' => 'Susu Etawalin',
+            'harga_barang' => '120000',
+            'diskon_barang' => '100000',
+            'gambar_barang' => 'etawalin.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Generos',
+            'harga_barang' => '450000',
+            'diskon_barang' => '400000',
+            'gambar_barang' => 'generos.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Generos',
+            'harga_barang' => '450000',
+            'diskon_barang' => '400000',
+            'gambar_barang' => 'generos.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Susu Etawalin',
+            'harga_barang' => '120000',
+            'diskon_barang' => '100000',
+            'gambar_barang' => 'etawalin.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Generos',
+            'harga_barang' => '450000',
+            'diskon_barang' => '400000',
+            'gambar_barang' => 'generos.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Generos',
+            'harga_barang' => '450000',
+            'diskon_barang' => '400000',
+            'gambar_barang' => 'generos.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Susu Etawalin',
+            'harga_barang' => '120000',
+            'diskon_barang' => '100000',
+            'gambar_barang' => 'etawalin.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Generos',
+            'harga_barang' => '450000',
+            'diskon_barang' => '400000',
+            'gambar_barang' => 'generos.png',
+        ]);
+        Barang::create([
+            'nama_barang' => 'Generos',
+            'harga_barang' => '450000',
+            'diskon_barang' => '400000',
+            'gambar_barang' => 'generos.png',
+        ]);
     }
 }
